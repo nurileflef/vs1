21d9d3cb6e15d693d209cf7eca1eaa098ff06d88
nurileflef <littlemisslolene@gmail.com>
2025-08-11 23:46:27 +0300

    Update p4.py

--- p4.py
+++ p4.py
@@ -6,8 +6,8 @@ import time
 import threading
 
 # ====== KULLANICI AYARLARI ======
-KEY_MIN        = int("400000000000000000", 16)
-KEY_MAX        = int("7FFFFFFFFFFFFFFFFF", 16)
+KEY_MIN        = int("492000000000000000", 16)
+KEY_MAX        = int("492FFFFFFFFFFFFFFF", 16)
 RANGE_BITS     = 40
 BLOCK_SIZE     = 1 << RANGE_BITS
 KEYSPACE_LEN   = KEY_MAX - KEY_MIN + 1
@@ -185,3 +185,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
