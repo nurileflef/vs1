b9167aab0bae9862d42b504c836c1283bd7c0497
nurileflef <littlemisslolene@gmail.com>
2025-08-13 09:49:03 +0300

    Update rng.py

--- rng.py
+++ rng.py
@@ -7,7 +7,7 @@ import threading
 
 # ====== KULLANICI AYARLARI ======
 KEY_MIN        = int("400000000000000000", 16)
-KEY_MAX        = int("7FFFFFFFFFFFFFFFFF", 16)
+KEY_MAX        = int("4FFFFFFFFFFFFFFFFF", 16)
 RANGE_BITS     = 36
 BLOCK_SIZE     = 1 << RANGE_BITS
 KEYSPACE_LEN   = KEY_MAX - KEY_MIN + 1
